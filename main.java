5efa84573561d6d32754c715030a4d06a4fa5989
DELL <jamgirovaruxsora@gmail.com>
2023-01-07 20:04:03 +0500

    new project

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] ags) {
+        System.out.println("hello world");
+        System.out.println("sum");
+        System.out.println(sum(5, 7));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
